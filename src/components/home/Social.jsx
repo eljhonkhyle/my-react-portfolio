22a7d996e94dc49e94efd3f15a3712d5521188cc
Ej <101543580+secret2213@users.noreply.github.com>
2023-04-03 01:50:25 +0800

    Initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/eljhonkhylee/" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.twitter.com/EljhonKhyle/" className="home__social-icon" target="_blank">
+        <i className="uil uil-twitter-alt"></i>
+        </a>
+
+        <a href="https://www.github.com/eljhonkhyle/" className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
