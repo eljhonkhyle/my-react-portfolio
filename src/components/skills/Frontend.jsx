a13aa384ccc4c767f4ae936bb46a1f702cf85b56
Ej <101543580+secret2213@users.noreply.github.com>
2023-06-17 22:46:32 +0800

    commit

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -30,7 +30,7 @@ const Frontend = () => {
 
                     <div>
                         <h3 className="skills__name">Javascript</h3>
-                        <span className="skills__level">Intermidiate</span>
+                        <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
             </div>
@@ -41,7 +41,7 @@ const Frontend = () => {
 
                     <div>
                         <h3 className="skills__name">Bootstrap</h3>
-                        <span className="skills__level">Intermidiate</span>
+                        <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
 
@@ -50,7 +50,7 @@ const Frontend = () => {
 
                     <div>
                         <h3 className="skills__name">Git</h3>
-                        <span className="skills__level">Intermidiate</span>
+                        <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
 
@@ -59,7 +59,7 @@ const Frontend = () => {
 
                     <div>
                         <h3 className="skills__name">React</h3>
-                        <span className="skills__level">Intermidiate</span>
+                        <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
             </div>
